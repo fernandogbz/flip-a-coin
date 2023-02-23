2f5fffe4a2d75559f4e0a33149547957c94709ba
Fernando Jimenez <fgjv06@gmail.com>
2023-02-23 08:26:09 -0300

    JS - flip coin on button click if

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ let resetBtn = document.querySelector("#reset-button");
 flipBtn.addEventListener("click", () => {
   let i = Math.floor(Math.random() * 2);
   coin.style.animation = "none";
+    if(i) {
+      setTimeout(function(){
+        coin.style.animation = "spin-heads 3s forwards";
+      }, 100);
+      heads++;
+    } else {
+      setTimeout(function(){
+        coin.style.animation = "spin-tails 3s forwards";
+      }, 100);
+      tails++;
+    }
 });
